ee70af1d97d66257cafc623aed7f1a408d070899
Ashutosh Pandey <52138864+javabyashutosh@users.noreply.github.com>
2021-08-20 02:06:26 +0530

    Max and Min Element Finder in Integer Array Using Stream Api

--- CoreJava/src/com/corejava/java8/FindLargestElementInPrimitiveArrayUsingStreamAPI.java
+++ CoreJava/src/com/corejava/java8/FindLargestElementInPrimitiveArrayUsingStreamAPI.java
@@ -5,30 +5,15 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class FindLargestElementInPrimitiveArrayUsingStreamAPI {
+public class FindMinMaxElementInPrimitiveArrayUsingStreamAPI {
 
 	public static void main(String[] args) {
 
-		//Creating Integer Array of size:5
-		int[] intArray= {1,2,3,4,5,6,7,8,9,10};
+		int[] intArray = { 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int min = Arrays.stream(intArray).min().getAsInt();
+		System.out.println("min element in given array:"+min);
 		
-		List<int[]> intArrayList=Arrays.asList(intArray);
-		
-		//intArrayList.stream().max(Comparator.comparing(Integer::valueOf)).get();
-		//Iterate List using forEach method
-		intArrayList.forEach(i->{
-			int max=0;
-			IntStream integerArrayStream=Arrays.stream(intArray);
-			//Iterate Array using Enhanced For Loop
-			for(int i1:i) {
-				max=IntStream.of(i1).max().getAsInt();
-				System.out.println("maximum::"+max);
-
-			}
-			System.out.println("max value::"+max);
-		});
-		
-		
+		int max=Arrays.stream(intArray).max().getAsInt();
+		System.out.println("max element in given array:"+max);	
 	}
-
 }
